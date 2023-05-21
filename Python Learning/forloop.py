d98546144ae29822c3886c91612ad77ba54a40d3
AdBinay <binayadh77@gmail.com>
2023-05-22 00:47:53 +0545

    For loop

---
+++ Python Learning/forloop.py
@@ -0,0 +1,18 @@
+name = 'binay'
+for i in name:
+    print(i)
+
+#for loop in lists
+sub = ['math','sci','soc','eng','nep']
+for i in sub:
+    print(i)
+    if(i=="sci"):
+        print("this is easy")
+
+
+#print the sum of first 10 number
+Sum = 0
+for num in range(10):
+    Sum += num
+
+print(Sum)
