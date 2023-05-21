0c41df317e4c344d155e6f417b8577ee494e6f54
AdBinay <binayadh77@gmail.com>
2023-05-22 01:00:52 +0545

    while loop

---
+++ while.py
@@ -0,0 +1,20 @@
+'''While loop executes until the condition is true'''
+
+
+#first 10 even numbers
+i = 0 
+while(i<20):
+    print(i)
+    i=i+2
+
+
+
+
+
+#decremental while loop
+count = 5
+while(count>0):
+    print(count)
+    count = count - 1
+else:
+    print('while loop terminated')
