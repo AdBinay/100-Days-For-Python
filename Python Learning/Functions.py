b49bff1ca90b55a7476fcb516f9603ebc13c7567
AdBinay <binayadh77@gmail.com>
2023-05-22 14:12:02 +0545

    Function

---
+++ Python Learning/Functions.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+
+total = add(2,3)
+print(total)
+
+
+
+def greet(name='there', message='Hi'):
+    return f"{message} {name}"
+
+
+greeting = greet('binay')
+print(greeting)
